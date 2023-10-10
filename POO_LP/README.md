4cd81d52618bea15bb8957c50d99acf707a458a7
axelyadieldiaz <131999776+axelyadieldiaz@users.noreply.github.com>
2023-10-10 12:32:08 -0500

    clase del dia 10 de octubre

--- POO_LP/README.md
+++ POO_LP/README.md
@@ -8,3 +8,38 @@ POO es un modelo de como programar
 
 se basa en objetos
 y el la POO un objeto es toda entidad que se puede escribir atravez de **atributos** y **funciones** que pueda realizar la entidad
+
+```python
+class celular:
+    #atributos de tipo clase
+    # que son iguales para todos los objetos
+    # que se creen
+    familia='smart phone'
+    
+    #atributos de intancia
+    #son atributos propios del objeto 
+    #creamos una funcion inicializadora
+    def __init__(self,marca,modelo,imei,nroCelular):
+        self.marca=marca
+        self=modelo=modelo
+        self.imei=imei
+        self.nroCelular.nroCelular
+
+
+    #funcionalidades
+    def llamar(self,destino):
+        return f'llamando a {destino}'
+
+#objeto celular jory
+llamandojory=Celular('poco','x5','243454565','4352323324') #instanciando mi clase - creando mi objeto celular
+print(llamandojory.marca)
+print(llamandojory.familia)
+print(llamandojory.llamar('china'))
+
+#objeto celular nadine
+llamandonadine=Celular('alcatel','basico','2456785432','6433223423')
+print(llamandonadine.marca)
+print(llamandonadine.familia)
+print(llamandonadine.llmar('ollanta'))
+
+ ```
--- POO_LP/ejercicios.py
+++ POO_LP/ejercicios.py
@@ -103,3 +103,95 @@ class factura:
         return "impuesto general de la venta"
     
 print(factura.nombre)
+
+## 1. crear un objeto CLASE laptop con dos atributos de clase y 5 atributos de instancia
+# debera tener hasta 3 funcionalidades como minimo
+#crear tres objetos instancia de clase distintos
+### ojo: solo utilizar lo que hemos echo en clase
+class laptop:
+    familia1='intel CORE'
+    familia2='16.5 pulgadas'
+
+    def __init__(self,marca,modelo,microprocesador,memoria_RAM,HDMI):
+        self.marca=marca
+        self=modelo=modelo
+        self.microprocesador=microprocesador
+        self.memoria_RAM=memoria_RAM
+        self.HDMI=HDMI
+
+    def encerder(self,):
+        print(f'laptop {self.modelo} se esta encendiendo...')
+    def apagar(self,):
+        print(f'la laptop {self.modelo}se esta apagando...')
+    
+    def mostrar_informacion(self):
+        print(f'imformacion de la laptop {self.modelo}:')
+        print(f'marca: {self.marca}')
+        print(f'modelo: {self.modelo}')
+        print(f'memoria_RAM: {self.memoria_RAM}')
+        print(f'HDMI: {self.HDMI}')
+
+
+laptop1 = laptop("dell XPS 13", 1300, 15, 512, 13.3, "plateado")
+laptop2 = laptop("HP Spectre x360", 1300, 10, 256, 15.6, "gris")
+laptop3 = laptop("lenovo trinkpad", 900, 12, 256, 15, "negro")
+
+laptop1.encerder()
+laptop1.mostrar_informacion()
+laptop1.apagar()
+
+laptop2.encerder()
+laptop2.mostrar_informacion()
+laptop2.apagar()
+
+laptop3.encerder()
+laptop3.mostrar_informacion()
+laptop3.apagar()
+
+
+## crear una clase puesto de mercado que tenga un atributo de clase 5 atributos de instancia y 5 funcionalidades
+# debera crear 4 instancias de la clase mercado
+## ejemplo puesto mechita, puesto la gringa, puesto ojo de uva
+class Mercado:
+    medida="establecimiento"
+
+    def _init_(self,nombre_puesto,sexo,color,tipo_de_puesto,productos):
+        self.nombre_puesto=nombre_puesto
+        self.sexo=sexo
+        self.color=color
+        self.tipo_de_puesto=tipo_de_puesto
+        self.productos=productos
+
+    def entrada_de_cliente(self,nombre):
+        return f"el cliente {nombre} entro"
+    
+    def compro(self,client,product):
+        return f"{client} compro este producto: {product}"
+    
+    def salir(self,client):
+        return f"el client {client} salio por la puerta"
+    
+# objeto 1
+clientvirgen=Mercado("puesto mechita","femeninio","azul","comida","cebiche")
+print(clientvirgen.tipo_de_puesto)
+print(clientvirgen.productos)
+print(clientvirgen.entrada_de_cliente("orlando"))
+print(clientvirgen.compro("orlando","cebiche"))
+# objeto 2
+clientjona=Mercado("la gringa","femeninio","rosa","venta de celulares","S23 Ultra")
+print(clientjona.tipo_de_puesto)
+print(clientjona.productos)
+print(clientjona.entrada_de_cliente("jhonatan devorador de mundos"))
+print(clientjona.compro("jhonatan","S23 Ultra"))
+# objeto 3
+clientjona=Mercado("la gringa","femeninio","rosa","venta de celulares","S23 Ultra")
+print(clientjona.tipo_de_puesto)
+print(clientjona.productos)
+print(clientjona.entrada_de_cliente("jhonatan devorador de mundos"))
+print(clientjona.salir("hans"))
+# objeto 4
+clientchina=Mercado("los facheros","masculino","naranja","venta de ropa","zapatos")
+print(clientchina.tipo_de_puesto)
+print(clientchina.productos)
+print(clientchina.entrada_de_cliente("jhonatan devorador de mundos"))
+print(clientchina.salir("china"))
